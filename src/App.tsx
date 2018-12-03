f53936a2cb933955aac9ffbb069a62dbb5db2fee
luowendi <15622285015@163.com>
2018-12-03 20:38:35 +0800

    update node_module

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import './App.scss';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <span className="App-name">aaa</span>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './index.scss';
+import './common/scss/reset.scss'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
