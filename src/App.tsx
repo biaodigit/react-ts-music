e0a5f899dd58b2063f645d54cec78d747ec739b9
luowendi <15622285015@163.com>
2018-12-05 23:28:49 +0800

    add api config

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import './App.scss';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <span className="App-name">aaa</span>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <span className="App-name">aaa</span>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/utils/config.ts
@@ -0,0 +1,5 @@
+export default {
+    url: {
+        baseUrl: process.env.NODE_ENV === 'development' ? 'http://192.168.0.100:3000/' : '/'
+    }
+}
