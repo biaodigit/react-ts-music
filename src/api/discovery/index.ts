063f1b894a782452a706501c995a9dc14758ae3b
luowendi <15622285015@163.com>
2018-12-23 10:30:09 +0800

    update discovery api

--- src/api/discovery/index.ts
+++ src/api/discovery/index.ts
@@ -1,12 +1,46 @@
 import fetch from '../../utils/fetch'
 
 /**
- * 获取轮播图
+ * 轮播图
  * @returns {AxiosPromise}
  */
 export function getBanner() {
     return fetch({
         method: 'get',
         url: 'banner'
+    })
+}
+
+/**
+ * 全部精品歌单
+ * @returns {AxiosPromise}
+ */
+export function getFineSongList() {
+    return fetch({
+        method: 'get',
+        url: 'top/playlist'
+    })
+}
+
+/**
+ * 推荐歌单
+ * @returns {AxiosPromise}
+ */
+export function getRecSongList() {
+    return fetch({
+        method: 'get',
+        url: 'personalized'
+    })
+}
+
+/**
+ * 歌单详情
+ * @param {number} id
+ * @returns {AxiosPromise}
+ */
+export function getSongListDetail(id: number) {
+    return fetch({
+        method: 'get',
+        url: `playlist/detail?id=${id}`
     })
 }
